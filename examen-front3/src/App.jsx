8545645347c9a8443849a8e456e7754457b6ecc9
anabellagallo <agallo3759@gmail.com>
2023-03-07 11:07:30 -0300

    agregar cambios

--- examen-front3/src/App.jsx
+++ examen-front3/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css"
 import Card from './componentes/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
